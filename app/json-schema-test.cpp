2ee354a43dd86b58e2f9f20daaaf33666f555714
Patrick Boettcher <patrick.boettcher@posteo.de>
2016-12-28 13:48:22 +0100

    test: format regex for new optional test (doens't work)

--- app/json-schema-test.cpp
+++ app/json-schema-test.cpp
@@ -48,6 +48,12 @@ static void format_check(const std::string &format, const std::string &value)
 		std::regex re(R"((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))");
 		if (!std::regex_match(value, re))
 			throw std::invalid_argument(value + " is not a IPv6-address.");
+	} else if (format == "regex") {
+		try {
+			std::regex re(value, std::regex::ECMAScript);
+		} catch (std::exception &e) {
+			throw e;
+		}
 	} else
 		throw std::logic_error("don't know how to validate " + format);
 }
