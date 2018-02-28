8e046bd28ce8b5624434dea51fbd70dde625e50c
Ian Bell <ian.h.bell@gmail.com>
2018-02-27 21:53:36 -0700

    Fix preprocessor branches for dllimport

--- src/json-schema.hpp
+++ src/json-schema.hpp
@@ -27,10 +27,12 @@
 #define NLOHMANN_JSON_SCHEMA_HPP__
 
 #ifdef _WIN32
-#    ifdef JSON_SCHEMA_VALIDATOR_EXPORTS
+#    if defined(JSON_SCHEMA_VALIDATOR_EXPORTS)
 #        define JSON_SCHEMA_VALIDATOR_API __declspec(dllexport)
-#    else
+#    elif defined(JSON_SCHEMA_VALIDATOR_IMPORTS)
 #        define JSON_SCHEMA_VALIDATOR_API __declspec(dllimport)
+#    else
+#        define JSON_SCHEMA_VALIDATOR_API
 #    endif
 #else
 #    define JSON_SCHEMA_VALIDATOR_API
