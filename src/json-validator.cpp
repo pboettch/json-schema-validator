0a2cfd0caa21b7c12a5d4e22241faf787f1d4a5e
Sylvain Joubert <joubert.sy@gmail.com>
2023-07-20 17:24:04 +0200

    Fix performance regression from logical combination patch discard
    A recent fix to discard patch from invalid logical combination
    introduced a lot (for large json instance and/or schema) of copies to
    make a patch backup. On some case, this introduced up to a 100x slower
    validation time.
    
    Resizing the patch object to its previous size avoid unnecessary
    temporary allocations from the backup object.

--- src/json-validator.cpp
+++ src/json-validator.cpp
@@ -436,12 +436,12 @@ class logical_combination : public schema
 
 		for (auto &s : subschemata_) {
 			first_error_handler esub;
-			json_patch old_patch(patch);
+			auto oldPatchSize = patch.get_json().size();
 			s->validate(ptr, instance, patch, esub);
 			if (!esub)
 				count++;
 			else
-				patch = old_patch;
+				patch.get_json().get_ref<nlohmann::json::array_t &>().resize(oldPatchSize);
 
 			if (is_validate_complete(instance, ptr, e, esub, count))
 				return;
